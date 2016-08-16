37d56ae0b6efbb8f5ec770a191ed978394388e6d
Martin Panter <vadmium  gmailcom>
2016-08-16 01:44:31 +0000

    Split out cached downloading and scraping functions

--- shop.py
+++ shop.py
@@ -24,48 +24,8 @@ class main:
             urlopen = urllib.request.build_opener(handler).open
             url = "https://au.rs-online.com/web/c/" \
                 "connectors/pcb-connectors/pcb-pin-socket-strips/"
-            print(end="GET {} ".format(url), flush=True, file=stderr)
-            split = urlsplit(url)
-            path = split.path.split("/")
-            dir = os.path.join(split.scheme, split.netloc, *path[:-1])
-            suffix = hashlib.md5()
-            suffix.update(url.encode())
-            suffix = urlsafe_b64encode(suffix.digest()[:6]).decode("ascii")
-            if path[-1]:
-                suffix = path[-1] + os.extsep + suffix
-            suffix += os.extsep
-            metadata = os.path.join(dir, suffix + "mime")
             with ExitStack() as cleanup:
-                try:
-                    metadata = open(metadata, "rb")
-                except FileNotFoundError:
-                    types = ("text/html",)
-                    os.makedirs(dir, exist_ok=True)
-                    suffix += "html"
-                    cache = open(os.path.join(dir, suffix), "xb")
-                    cleanup.enter_context(cache)
-                    with open(metadata, "xb") as metadata:
-                        response = http_request(url, types, urlopen=urlopen)
-                        cleanup.enter_context(response)
-                        print(response.status, response.reason,
-                            flush=True, file=stderr)
-                        msg = Message()
-                        msg.add_header("Content-Type",
-                            "message/external-body; access-type=local-file")
-                        msg.set_param("name", suffix)
-                        msg.attach(response.info())
-                        metadata = email.generator.BytesGenerator(metadata,
-                            mangle_from_=False, maxheaderlen=0)
-                        metadata.flatten(msg)
-                    msg = response.info()
-                    response = TeeReader(response, cache.write)
-                else:
-                    with metadata:
-                        msg = email.message_from_binary_file(metadata)
-                    cache = os.path.join(dir, msg.get_param("name"))
-                    [msg] = msg.get_payload()
-                    response = cleanup.enter_context(open(cache, "rb"))
-                    print("(cached)", flush=True, file=stderr)
+                [msg, response] = get_cached(url, urlopen, cleanup)
                 charset = msg.get_content_charset()
                 response = TextIOWrapper(response, charset)
                 cleanup.enter_context(response)
@@ -81,69 +41,109 @@ class main:
             
             with rewrap(stdout, newline="") as out:
                 out = csv.writer(out)
-                header = None
-                for elem in response.iterfind(".//table[@class]"):
-                    if "srtnTblHeader" in elem.get("class").split():
-                        assert header is None
-                        header = elem
-                header = header.find(".//tr")
-                header = ("".join(cell.itertext()).strip()
-                    for cell in header.iter("td"))
-                
-                out_header = ["href", "prodDesc"]
-                out_header.append(next(header))
-                out_header.extend(("packaging", "qty"))
-                part_header = list()
-                cell = next(header)
-                assert cell == "Part Details"
-                header = list(header)
-                
-                [table] = response.iterfind(".//table[@class='srtnListTbl']")
-                for [i, row] in enumerate(table.iterfind(".//tr")):
-                    row = row.iterfind(".//td")
-                    cell = next(row)
-                    
-                    [desc] = cell.iterfind(".//a[@class='tnProdDesc']")
-                    out_row = [desc.get("href")]
-                    out_row.append("".join(desc.itertext()).strip())
-                    
-                    pricing = None
-                    for elem in cell.iterfind(".//span[1][@class]/.."):
-                        if "price" in elem[0].get("class", "").split():
-                            assert pricing is None
-                            pricing = elem
-                    pricing = iter(pricing)
-                    out_row.append("".join(next(pricing).itertext()))
-                    pricing = "".join(t
-                        for elem in pricing for t in elem.itertext())
-                    out_row.append(pricing)
-                    
-                    [qty] = cell.iterfind(".//*[@class='qty']//input")
-                    qty = int(qty.get("value"))
-                    out_row.append(qty)
-                    if qty == 1:
-                        expected = ("Each", "1 ")
-                    else:
-                        expected = "Each (In a Pack of {})".format(qty)
-                    assert pricing.startswith(expected), (qty, pricing)
-                    
-                    cell = next(row)
-                    labels = list()
-                    for detail in cell.iterfind(".//li"):
-                        labels.append("".join(detail[0].itertext()))
-                        text = "".join(t
-                            for elem in detail[1:] for t in elem.itertext())
-                        out_row.append(text.strip())
-                    
-                    out_row.extend("".join(cell.itertext()).strip()
-                        for cell in row)
-                    
-                    if i:
-                        assert labels == part_header
-                    else:
-                        part_header = labels
-                        out.writerow(out_header + part_header + header)
-                    out.writerow(out_row)
+                rows = scrape_table(response)
+                out.writerows(rows)
+
+def scrape_table(response):
+    header = None
+    for elem in response.iterfind(".//table[@class]"):
+        if "srtnTblHeader" in elem.get("class").split():
+            assert header is None
+            header = elem
+    header = header.find(".//tr")
+    header = ("".join(cell.itertext()).strip() for cell in header.iter("td"))
+    
+    out_header = ["href", "prodDesc"]
+    out_header.append(next(header))
+    out_header.extend(("packaging", "qty"))
+    part_header = list()
+    cell = next(header)
+    assert cell == "Part Details"
+    header = list(header)
+    
+    [table] = response.iterfind(".//table[@class='srtnListTbl']")
+    for [i, row] in enumerate(table.iterfind(".//tr")):
+        row = row.iterfind(".//td")
+        cell = next(row)
+        
+        [desc] = cell.iterfind(".//a[@class='tnProdDesc']")
+        out_row = [desc.get("href")]
+        out_row.append("".join(desc.itertext()).strip())
+        
+        pricing = None
+        for elem in cell.iterfind(".//span[1][@class]/.."):
+            if "price" in elem[0].get("class", "").split():
+                assert pricing is None
+                pricing = elem
+        pricing = iter(pricing)
+        out_row.append("".join(next(pricing).itertext()))
+        pricing = "".join(t for elem in pricing for t in elem.itertext())
+        out_row.append(pricing)
+        
+        [qty] = cell.iterfind(".//*[@class='qty']//input")
+        qty = int(qty.get("value"))
+        out_row.append(qty)
+        if qty == 1:
+            expected = ("Each", "1 ")
+        else:
+            expected = "Each (In a Pack of {})".format(qty)
+        assert pricing.startswith(expected), (qty, pricing)
+        
+        cell = next(row)
+        labels = list()
+        for detail in cell.iterfind(".//li"):
+            labels.append("".join(detail[0].itertext()))
+            text = "".join(t for elem in detail[1:] for t in elem.itertext())
+            out_row.append(text.strip())
+        
+        out_row.extend("".join(cell.itertext()).strip() for cell in row)
+        
+        if i:
+            assert labels == part_header
+        else:
+            part_header = labels
+            yield out_header + part_header + header
+        yield out_row
+
+def get_cached(url, urlopen, cleanup):
+    print(end="GET {} ".format(url), flush=True, file=stderr)
+    split = urlsplit(url)
+    path = split.path.split("/")
+    dir = os.path.join(split.scheme, split.netloc, *path[:-1])
+    suffix = hashlib.md5(url.encode()).digest()[:6]
+    suffix = urlsafe_b64encode(suffix).decode("ascii")
+    if path[-1]:
+        suffix = path[-1] + os.extsep + suffix
+    suffix += os.extsep
+    metadata = os.path.join(dir, suffix + "mime")
+    try:
+        metadata = open(metadata, "rb")
+    except FileNotFoundError:
+        os.makedirs(dir, exist_ok=True)
+        suffix += "html"
+        cache = open(os.path.join(dir, suffix), "xb")
+        cleanup.enter_context(cache)
+        with open(metadata, "xb") as metadata:
+            types = ("text/html",)
+            response = http_request(url, types, urlopen=urlopen)
+            cleanup.enter_context(response)
+            print(response.status, response.reason, flush=True, file=stderr)
+            msg = Message()
+            msg.add_header("Content-Type",
+                "message/external-body; access-type=local-file",
+                name=suffix)
+            msg.attach(response.info())
+            metadata = email.generator.BytesGenerator(metadata,
+                mangle_from_=False, maxheaderlen=0)
+            metadata.flatten(msg)
+        return (response.info(), TeeReader(response, cache.write))
+    with metadata:
+        msg = email.message_from_binary_file(metadata)
+    cache = os.path.join(dir, msg.get_param("name"))
+    [msg] = msg.get_payload()
+    response = cleanup.enter_context(open(cache, "rb"))
+    print("(cached)", flush=True, file=stderr)
+    return (msg, response)
 
 class HtmlTreeParser(HTMLParser):
     def __init__(self):
